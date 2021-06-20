3327e656ab3767091afee1743aed9c525868bffd
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2021-06-20 23:27:32 +0300

    feat: connect get_it lib

---
+++ lib/locator_service.dart
@@ -0,0 +1,53 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:rick_and_morty/core/platform/network_info.dart';
+import 'package:rick_and_morty/feature/data/datasources/person_local_data_source.dart';
+import 'package:rick_and_morty/feature/data/datasources/person_remote_data_source.dart';
+import 'package:rick_and_morty/feature/data/repositories/person_repository_impl.dart';
+import 'package:rick_and_morty/feature/domain/repositories/person_repository.dart';
+import 'package:rick_and_morty/feature/domain/usecases/get_all_persons.dart';
+import 'package:rick_and_morty/feature/domain/usecases/search_person.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/person_list_cubit/person_list_cubit.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/search_bloc/search_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Bloc/Cubit
+  sl.registerFactory(() => PersonListCubit(getAllPersons: sl()));
+  sl.registerFactory(() => PersonSearchBloc(searchPerson: sl()));
+
+  // UseCases
+  sl.registerLazySingleton(() => GetAllPersons(sl()));
+  sl.registerLazySingleton(() => SearchPerson(sl()));
+
+  // Repos
+  sl.registerLazySingleton<PersonRepository>(
+    () => PersonRepositoryImpl(
+      remoteDateSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton<PersonRemoteDateSource>(
+    () => PersonRemoteDateSourceImpl(client: http.Client()),
+  );
+
+  sl.registerLazySingleton<PersonLocalDataSource>(
+    () => PersonLocalDataSourceImp(sharedPreferences: sl()),
+  );
+
+  // Core
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(internetConnectionChecker: sl()),
+  );
+
+  // External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() async => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
