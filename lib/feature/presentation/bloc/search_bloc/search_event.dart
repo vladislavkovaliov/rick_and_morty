b31ca2aa378ced1e639b574cf43104061666fb44
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2021-06-20 16:35:56 +0300

    bloc: implement search (#7)

---
+++ lib/feature/presentation/bloc/search_bloc/search_event.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+abstract class PersonSearchEvent extends Equatable {
+  const PersonSearchEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class SearchPersons extends PersonSearchEvent {
+  final String personQuery;
+
+  const SearchPersons({
+    required this.personQuery,
+  });
+}
---
+++ lib/feature/presentation/bloc/search_bloc/search_state.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+
+abstract class PersonSearchState extends Equatable {
+  const PersonSearchState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class PersonEmpty extends PersonSearchState {}
+
+class PersonSearchLoading extends PersonSearchState {}
+
+class PersonSearchLoaded extends PersonSearchState {
+  final List<PersonEntity> persons;
+
+  const PersonSearchLoaded({
+    required this.persons,
+  });
+
+  @override
+  List<Object> get props => [persons];
+}
+
+class PersonSearchError extends PersonSearchState {
+  final String message;
+
+  const PersonSearchError({
+    required this.message,
+  });
+
+  @override
+  List<Object> get props => [message];
+}
