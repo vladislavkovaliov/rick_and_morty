419bc57e1feae13040afca5c65ed6e603ba9160d
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2021-06-20 14:11:22 +0300

    domain: create entities, repos, usecases

---
+++ lib/feature/domain/usecases/search_person.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:rick_and_morty/core/error/failure.dart';
+import 'package:rick_and_morty/core/usecases/usecases.dart';
+import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+import 'package:rick_and_morty/feature/domain/repositories/person_repository.dart';
+
+class SearchPerson extends UseCase<List<PersonEntity>, SearchPersonParams> {
+  final PersonRepository personRepository;
+
+  SearchPerson(this.personRepository);
+
+  @override
+  Future<Either<Failure, List<PersonEntity>>> call(SearchPersonParams params) async {
+    return await personRepository.searchPerson(params.query);
+  }
+}
+
+class SearchPersonParams extends Equatable {
+  final String query;
+
+  const SearchPersonParams({
+      required this.query,
+  });
+use
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
